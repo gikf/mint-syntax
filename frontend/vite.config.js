e361ec801e3c6c5e2290c44781392a66249634d8
Krzysztof G. <60067306+gikf@users.noreply.github.com>
2025-07-10 21:30:51 +0200

    chore: remove vite server config (#49)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,9 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '../', 'VITE_');
-  return {
-    envDir: '../',
-    plugins: [tailwindcss(), react()],
-    server: {
-      cors: {
-        origin: JSON.stringify(env.VITE_HOME_LOCATION),
-      },
-    },
-  };
+export default defineConfig({
+  envDir: '../',
+  plugins: [tailwindcss(), react()],
 });
