b3972ad5842a040e389da60275dd09de5d1fb3a1
Krzysztof G. <60067306+gikf@users.noreply.github.com>
2025-07-18 13:31:33 +0200

    feat: load idea data from api (#123)

--- frontend/src/pages/Ideas/IdeaPage.jsx
+++ frontend/src/pages/Ideas/IdeaPage.jsx
@@ -1,8 +1,40 @@
+import { useEffect } from 'react';
 import { useParams } from 'react-router';
+
+import { useApi } from '../../hooks/useApi';
+import Spinny from '../../components/Spinny';
 
 export const IdeaPage = () => {
   const { ideaId } = useParams();
-  return <>{ideaId}</>;
+  const { isLoading, error, data, fetchFromApi } = useApi({
+    loadingInitially: true,
+  });
+
+  useEffect(() => {
+    fetchFromApi(`/ideas/${ideaId}`);
+  }, [fetchFromApi, ideaId]);
+
+  return isLoading ? (
+    <Spinny />
+  ) : error ? (
+    <>{error}</>
+  ) : (
+    <section>
+      <h1 className='header'>{data?.name}</h1>
+      <p>{data?.description}</p>
+      <div className='stats stats-vertical lg:stats-horizontal shadow'>
+        <div className='stat'>
+          <div className='stat-title'>Upvotes</div>
+          <div className='stat-value'>{data?.upvoted_by.length}</div>
+        </div>
+
+        <div className='stat'>
+          <div className='stat-title'>Downvotes</div>
+          <div className='stat-value'>{data?.downvoted_by.length}</div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default IdeaPage;
