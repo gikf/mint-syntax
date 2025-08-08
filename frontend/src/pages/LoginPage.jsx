4ce994ccff6230b6409a2161a8c2515ce0c4723a
Krzysztof G. <60067306+gikf@users.noreply.github.com>
2025-08-08 20:37:14 +0200

    chore: get redirect query parameter for login page (#245)

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,13 +1,16 @@
-import { Link } from 'react-router';
+import { Link, useLocation } from 'react-router';
 import { LoginForm } from '../components/LoginForm';
 
 export const LoginPage = () => {
+  const { search } = useLocation();
+  const redirect_to = new URLSearchParams(search).get('redirect') ?? '/';
+
   return (
     <div className='container-wrapper'>
       <section className='section-card flex flex-col justify-center items-center'>
         <h3 className='section-heading'>Login to your account:</h3>
         <div className='card bg-base-100 w-lg p-4 auto'>
-          <LoginForm redirect_to='/' />
+          <LoginForm redirect_to={redirect_to} />
         </div>
 
         {/* ✍️ Register link */}
